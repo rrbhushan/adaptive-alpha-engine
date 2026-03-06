03e1f3e43487d855108413a22e28d46d9afbd818
rrbhushan <rrbhushan@ucsd.edu>
2026-03-06 10:41:00 -0800

    Add config scaffold and package structure

---
+++ src/__main__.py
@@ -0,0 +1,9 @@
+import argparse
+from .run_experiment import main
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True)
+    args = parser.parse_args()
+
+    main(args.config)
